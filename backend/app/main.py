5463a15cce58545b48b8435054d07fc7f52fdf98
Saurabh Jojare <saurabh.jojare@gmail.com>
2026-09-01 17:31:30 +0530

    Added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,4 +2,13 @@ from fastapi import FastAPI
 from app.api.send_message import router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
